93718a49a05ac5fed11b5fb2a39ab572e6a0137c
Scott MacLellan <s.j.maclell@gmail.com>
2016-02-12 12:37:38 -0500

    Better buttons and save on enter (shift to avoid)

--- client/src/components/EditParagraph.jsx
+++ client/src/components/EditParagraph.jsx
@@ -3,6 +3,24 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export const EditParagraph = React.createClass({
   mixins: [PureRenderMixin],
+  handleSave: function() {
+    this.props.saveParagraph(this.refs.edit.value, this.props.parent);
+  },
+  handleEnter: function(event) {
+    if(!event.shiftKey && event.keyCode == 13) {
+        this.handleSave();
+        return false;
+    }
+    return true;
+  },
+  componentDidMount: function(){
+    this.refs.add.focus();
+
+    this.refs.edit.addEventListener('keypress', this.handleEnter);
+  },
+  componentWillUnmount: function() {
+    this.refs.edit.addEventListener('keypress', this.handleEnter);
+  },
   render: function() {
 
     return <div className="paragraph paragraph-edit">
@@ -10,7 +28,7 @@ export const EditParagraph = React.createClass({
           <textarea ref="edit" name="edit" placeholder="Tell your story!" />
         </p>
         <div className="buttons">
-          <button onClick={() => this.props.saveParagraph(this.refs.edit.value,this.props.parent)}><span>Add</span></button>
+          <button ref="add" onClick={() => this.handleSave()}><span>Add</span></button>
           <button onClick={() => this.props.hideCreateParagraph(this.props.parent)}><span>Cancel</span></button>
         </div>
       </div>;
